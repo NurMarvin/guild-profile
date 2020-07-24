4ed18a9d60d76c0774385e1212744206f15ad07d
Marvin Witt <nurmarvin@protonmail.com>
2020-07-24 22:56:44 +0200

    Various improvements and bugfixes
    Adds the "Server Profile" button to the guild header drop down
    Closes https://github.com/NurMarvin/guild-profile/issues/1

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = class GuildProfile extends Plugin {
     powercord.api.i18n.loadAllStrings(i18n);
     this.loadStylesheet('styles.scss');
     this._injectContextMenu();
+    this._injectMenu();
   }
 
   requestMemberData(guildId) {
@@ -33,29 +34,69 @@ module.exports = class GuildProfile extends Plugin {
   }
 
   async _injectContextMenu() {
-    const menu = await getModule(['MenuItem']);
-    const mdl = await getModule(m => m.default && m.default.displayName === 'GuildContextMenu');
+    const { MenuGroup, MenuItem } = await getModule(['MenuItem']);
+    const GuildContextMenu = await getModule(m => m.default && m.default.displayName === 'GuildContextMenu');
 
     const requestMemberData = (guildId) => {
       return this.requestMemberData(guildId);
     }
 
-    inject('guild-profile', mdl, 'default', ([{ target, guild }], res) => {
-      if (target.tagName.toLowerCase() === 'a') {
-        res.props.children.splice(0, 0,
-          React.createElement(menu.MenuItem, {
-            id: 'guild-profile',
+    inject('guild-profile-context-menu', GuildContextMenu, 'default', ([{ guild }], res) => {
+      res.props.children.splice(0, 0,
+        React.createElement(MenuGroup, {},
+          React.createElement(MenuItem, {
+            key: 'guild-profile',
             label: Messages.GUILD_PROFILE,
             action: () => open(() => React.createElement(GuildProfileModal, { guild, section: 'GUILD_INFO', requestMemberData }))
           })
+        )
+      );
+      return res;
+    });
+    GuildContextMenu.default.displayName = 'GuildContextMenu';
+  }
+
+  async _injectMenu() {
+    const key = 'guild-profile';
+    const Menu = await getModule(['MenuItem']);
+    const { getGuild } = await getModule(['getGuild']);
+    const { getGuildId } = await getModule(['getGuildId']);
+
+    const requestMemberData = (guildId) => {
+      return this.requestMemberData(guildId);
+    }
+
+    inject('guild-profile-menu', Menu, 'default', ([{ children }], res) => {
+      const guild = getGuild(getGuildId());
+
+      if (res.props.id !== 'guild-header-popout') return res;
+
+      if (!children.some(group => {
+        const groupChildren = group.props.children;
+
+        if (typeof groupChildren === 'array') {
+          return groupChildren.some(item => item.key === key)
+        } else {
+          return groupChildren.key === key;
+        }
+      })) {
+        children.unshift(
+          React.createElement(Menu.MenuGroup, {},
+            React.createElement(Menu.MenuItem, {
+              key,
+              label: Messages.GUILD_PROFILE,
+              action: () => open(() => React.createElement(GuildProfileModal, { guild, section: 'GUILD_INFO', requestMemberData }))
+            })
+          )
         );
       }
       return res;
     });
-    mdl.default.displayName = 'GuildContextMenu';
+    Menu.default.displayName = 'Menu';
   }
 
   pluginWillUnload() {
-    uninject('guild-profile');
+    uninject('guild-profile-context-menu');
+    uninject('guild-profile-menu');
   }
 }
