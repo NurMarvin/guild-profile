4b181e85b4ef9c875eacb3e759ada2e770a81b88
Marvin Witt <17235016+NurMarvin@users.noreply.github.com>
2022-08-07 23:48:05 +0200

    fix: context menu injection

--- index.js
+++ index.js
@@ -3,41 +3,38 @@
  * Licensed under the Open Software License version 3.0
  */
 
-const { Plugin } = require('powercord/entities');
-const { inject, uninject } = require('powercord/injector');
+const { Plugin } = require("powercord/entities");
+const { inject, uninject } = require("powercord/injector");
 const {
   React,
   getModule,
   getModuleByDisplayName,
   FluxDispatcher,
   i18n: { Messages },
-} = require('powercord/webpack');
-const { open } = require('powercord/modal');
-const { findInReactTree } = require('powercord/util');
-const i18n = require('./i18n');
+} = require("powercord/webpack");
+const { open } = require("powercord/modal");
+const { findInReactTree } = require("powercord/util");
+const i18n = require("./i18n");
 
-const GuildProfileModal = require('./components/GuildProfileModal');
-const GuildProfileIcon = require('./components/GuildProfileIcon');
+const GuildProfileModal = require("./components/GuildProfileModal");
+const GuildProfileIcon = require("./components/GuildProfileIcon");
 
-const memberCountsStore = require('./memberCountsStore/store');
-const memberCountsActions = require('./memberCountsStore/actions');
+const memberCountsStore = require("./memberCountsStore/store");
+const memberCountsActions = require("./memberCountsStore/actions");
 
-const { getCurrentUser } = getModule([ 'getCurrentUser' ], false);
+const { getCurrentUser } = getModule(["getCurrentUser"], false);
 
 module.exports = class GuildProfile extends Plugin {
   async startPlugin() {
-    this.log('Icons provided by https://iconify.design/');
+    this.log("Icons provided by https://iconify.design/");
     powercord.api.i18n.loadAllStrings(i18n);
-    this.loadStylesheet('styles.scss');
-    this._injectOpenContextMenuLazy({
-      GuildContextMenu: this._injectContextMenu.bind(this)
-    });
+    this.loadStylesheet("styles.scss");
     this._injectMenu();
 
     this.handleMemberListUpdate = this.handleMemberListUpdate.bind(this);
 
     FluxDispatcher.subscribe(
-      'GUILD_MEMBER_LIST_UPDATE',
+      "GUILD_MEMBER_LIST_UPDATE",
       this.handleMemberListUpdate
     );
   }
@@ -56,7 +53,7 @@ module.exports = class GuildProfile extends Plugin {
         return;
       }
 
-      const { requestMembers } = getModule(['requestMembers'], false);
+      const { requestMembers } = getModule(["requestMembers"], false);
       requestMembers(id);
 
       const updateMemberCounts = (memberListUpdate) => {
@@ -69,14 +66,14 @@ module.exports = class GuildProfile extends Plugin {
         }
       }
 
-      FluxDispatcher.subscribe('GUILD_MEMBER_LIST_UPDATE', onReceived);
+      FluxDispatcher.subscribe("GUILD_MEMBER_LIST_UPDATE", onReceived);
     });
   }
 
   updateMemberCounts(memberListUpdate) {
     const { guildId, memberCount, groups } = memberListUpdate;
     const onlineCount = groups
-      .map((group) => (group.id != 'offline' ? group.count : 0))
+      .map((group) => (group.id != "offline" ? group.count : 0))
       .reduce((a, b) => {
         return a + b;
       }, 0);
@@ -86,60 +83,21 @@ module.exports = class GuildProfile extends Plugin {
     return memberCounts;
   }
 
-  _injectContextMenu() {
-    const { MenuGroup, MenuItem } = getModule(['MenuItem'], false);
-    const GuildContextMenu = getModule(
-      (m) => m.default && m.default.displayName === 'GuildContextMenu', false
-    );
-
-    const getMemberCounts = (guildId) => {
-      return this.getMemberCounts(guildId);
-    };
-
-    inject(
-      'guild-profile-context-menu',
-      GuildContextMenu,
-      'default',
-      ([{ guild }], res) => {
-        res.props.children.splice(
-          0,
-          0,
-          React.createElement(
-            MenuGroup,
-            {},
-            React.createElement(MenuItem, {
-              id: 'guild-profile',
-              key: 'guild-profile',
-              label: Messages.GUILD_PROFILE,
-              action: () =>
-                this._openModalHandler(() =>
-                  React.createElement(GuildProfileModal, {
-                    guild,
-                    section: 'GUILD_INFO',
-                    getMemberCounts
-                  })
-                )
-            })
-          )
-        );
-        return res;
-      }
-    );
-    GuildContextMenu.default.displayName = 'GuildContextMenu';
-  }
-
   async _injectMenu() {
-    const id = 'guild-profile';
-    const Menu = await getModule(['MenuItem']);
-    const { getGuild } = await getModule(['getGuild']);
-    const { getGuildId } = await getModule(['getLastSelectedGuildId']);
+    const id = "guild-profile";
+    const Menu = await getModule(["MenuItem"]);
+    const { getGuild } = await getModule(["getGuild"]);
+    const { getGuildId } = await getModule(["getLastSelectedGuildId"]);
 
     const getMemberCounts = (guildId) => {
       return this.getMemberCounts(guildId);
     };
 
-    inject('guild-profile-menu', Menu, 'default', ([{ children }], res) => {
-      if (res.props.children.props.id !== 'guild-header-popout') return res;
+    inject("guild-profile-menu", Menu, "default", ([{ children }], res) => {
+      const menuId = res.props.children.props.id;
+
+      if (menuId !== "guild-header-popout" && menuId !== "guild-context")
+        return res;
 
       if (!findInReactTree(res, (c) => c.props && c.props.id == id)) {
         children.unshift(
@@ -149,59 +107,33 @@ module.exports = class GuildProfile extends Plugin {
             React.createElement(Menu.MenuItem, {
               id,
               label: Messages.GUILD_PROFILE,
-              icon: () => React.createElement(GuildProfileIcon),
+              icon: () =>
+                menuId === "guild-header-popout"
+                  ? React.createElement(GuildProfileIcon)
+                  : null,
               action: () =>
                 this._openModalHandler(() =>
                   React.createElement(GuildProfileModal, {
                     guild: getGuild(getGuildId()),
-                    section: 'GUILD_INFO',
-                    getMemberCounts
+                    section: "GUILD_INFO",
+                    getMemberCounts,
                   })
-                )
+                ),
             })
           )
         );
       }
       return res;
     });
-    Menu.default.displayName = 'Menu';
+
+    Menu.default.displayName = "Menu";
   }
 
-  _injectOpenContextMenuLazy (menus) {
-    const module = getModule([ 'openContextMenuLazy' ], false);
+  _openModalHandler(element) {
+    const { openUserProfileModal } = getModule(["openUserProfileModal"], false);
+    const module = getModule(["openModalLazy"], false);
 
-    inject('guild-profile-context-lazy-menu', module, 'openContextMenuLazy', ([ event, lazyRender, params ]) => {
-      const warpLazyRender = async () => {
-        const render = await lazyRender(event);
-
-        return (config) => {
-          const menu = render(config);
-          const CMName = menu?.type?.displayName;
-
-          if (CMName) {
-            const moduleByDisplayName = getModuleByDisplayName(CMName, false);
-
-            if (CMName in menus) {
-              menus[CMName]();
-              delete menus[CMName];
-            }
-            if (moduleByDisplayName !== null) {
-              menu.type = moduleByDisplayName;
-            }
-          }
-          return menu;
-        };
-      };
-
-      return [ event, warpLazyRender, params ];
-    }, true);
-  }
-
-  _openModalHandler (element) {
-    const { openUserProfileModal } = getModule([ 'openUserProfileModal' ], false);
-    const module = getModule([ 'openModalLazy' ], false);
-
-    if (getModuleByDisplayName('UserProfileModal', false) !== null) {
+    if (getModuleByDisplayName("UserProfileModal", false) !== null) {
       open(element);
       return;
     }
@@ -211,53 +143,60 @@ module.exports = class GuildProfile extends Plugin {
     // but the user will not see the final UserProfileModal -
     // since it will be replaced at the last stage with our element
 
-    const { ModalRoot } = getModule([ 'ModalRoot' ], false);
+    const { ModalRoot } = getModule(["ModalRoot"], false);
     const userId = getCurrentUser().id;
 
-    inject('guild-profile-open-modal-lazy', module, 'openModalLazy', ([ initLazyRender ]) => {
-      const warpInitLazyRender = () => {
-        const lazyRender = initLazyRender();
+    inject(
+      "guild-profile-open-modal-lazy",
+      module,
+      "openModalLazy",
+      ([initLazyRender]) => {
+        const warpInitLazyRender = () => {
+          const lazyRender = initLazyRender();
 
-        return new Promise(async (resolve) => {
-          const render = await lazyRender;
+          return new Promise(async (resolve) => {
+            const render = await lazyRender;
 
-          resolve((event) => {
-            const res = render(event);
+            resolve((event) => {
+              const res = render(event);
 
-            if (res?.type?.displayName === 'UserProfileModal') {
-              const { props } = res;
-              const { root } = getModule([ 'root', 'body' ], false);
+              if (res?.type?.displayName === "UserProfileModal") {
+                const { props } = res;
+                const { root } = getModule(["root", "body"], false);
 
-              if (props.guildId === '@me' && props.user.id === userId) {
-                if (props.transitionState === 3) { // = close modal
-                  uninject('guild-profile-open-modal-lazy');
+                if (props.guildId === "@me" && props.user.id === userId) {
+                  if (props.transitionState === 3) {
+                    // = close modal
+                    uninject("guild-profile-open-modal-lazy");
+                  }
+
+                  return React.createElement(ModalRoot, {
+                    transitionState: props.transitionState,
+                    className: root,
+                    children: element(),
+                  });
                 }
-
-                return React.createElement(ModalRoot, {
-                  transitionState: props.transitionState,
-                  className: root,
-                  children: element()
-                });
               }
-            }
-            return res;
+              return res;
+            });
           });
-        });
-      };
+        };
 
-      return [ warpInitLazyRender ];
-    }, true);
+        return [warpInitLazyRender];
+      },
+      true
+    );
 
     openUserProfileModal({ userId });
   }
 
   pluginWillUnload() {
-    uninject('guild-profile-context-menu');
-    uninject('guild-profile-context-lazy-menu');
-    uninject('guild-profile-open-modal-lazy');
-    uninject('guild-profile-menu');
+    uninject("guild-profile-context-menu");
+    uninject("guild-profile-context-lazy-menu");
+    uninject("guild-profile-open-modal-lazy");
+    uninject("guild-profile-menu");
     FluxDispatcher.unsubscribe(
-      'GUILD_MEMBER_LIST_UPDATE',
+      "GUILD_MEMBER_LIST_UPDATE",
       this.handleMemberListUpdate
     );
   }
