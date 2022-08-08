4fd716406ef7788535b60dd8008676d481c2c3a8
Marvin Witt <17235016+NurMarvin@users.noreply.github.com>
2022-08-08 18:39:37 +0200

    chore: remove unused feature list

--- components/GuildProfileFeatureList.jsx
+++ components/GuildProfileFeatureList.jsx
@@ -26,58 +26,6 @@ module.exports = class GuildProfileFeatureList extends React.PureComponent {
   render() {
     const { guild, className } = this.props;
 
-    // TODO: Remove before release
-    // guild.features = new Set([
-    //   "ROLE_SUBSCRIPTIONS_AVAILABLE_FOR_PURCHASE",
-    //   "WELCOME_SCREEN_ENABLED",
-    //   "NEWS",
-    //   "COMMUNITY",
-    //   "MEMBER_VERIFICATION_GATE_ENABLED",
-    //   "PRIVATE_THREADS",
-    //   "PREVIEW_ENABLED",
-    //   "SEVEN_DAY_THREAD_ARCHIVE",
-    //   "THREADS_ENABLED",
-    //   "THREADS_ENABLED_TESTING",
-    //   "THREE_DAY_THREAD_ARCHIVE",
-    //   "VANITY_URL",
-    //   "PARTNERED",
-    //   "MONETIZATION_ENABLED",
-    //   "COMMERCE",
-    //   "ANIMATED_BANNER",
-    //   "BANNER",
-    //   "ROLE_ICONS",
-    //   "ANIMATED_ICON",
-    //   "MEMBER_PROFILES",
-    //   "VIP_REGIONS",
-    //   "ENABLED_DISCOVERABLE_BEFORE",
-    //   "MORE_EMOJI",
-    //   "VERIFIED",
-    //   "FEATURABLE",
-    //   "HAS_DIRECTORY_ENTRY",
-    //   "INVITE_SPLASH",
-    //   "DISCOVERABLE",
-    //   "NEW_THREAD_PERMISSIONS",
-    //   "CHANNEL_BANNER",
-    //   "TEXT_IN_VOICE_ENABLED",
-    //   "ROLE_SUBSCRIPTIONS_ENABLED_FOR_PURCHASE",
-    //   "ROLE_SUBSCRIPTIONS_ENABLED",
-    //   "PREMIUM_TIER_3_OVERRIDE",
-    //   "MORE_STICKERS",
-    //   "RELAY_ENABLED",
-    //   "INTERNAL_EMPLOYEE_ONLY",
-    //   "FORCE_RELAY",
-    //   "TICKETING_ENABLED",
-    //   "EXPOSED_TO_ACTIVITIES_WTP_EXPERIMENT",
-    //   "LINKED_TO_HUB",
-    //   "AUTO_MODERATION",
-    //   "BOOSTING_TIERS_EXPERIMENT_SMALL_GUILD",
-    //   "BOOSTING_TIERS_EXPERIMENT_MEDIUM_GUILD",
-    //   "HAD_EARLY_ACTIVITIES_ACCESS",
-    //   "TICKETED_EVENTS_ENABLED",
-    //   "BOT_DEVELOPER_EARLY_ACCESS",
-    //   "GUILD_HOME_TEST",
-    // ]);
-
     return (
       <div
         aria-label="Guild Features"
